717939aa6797f8d61e962c4d1eba3a3e0fd983c4
Sean <sxfmao@163.com>
2023-12-08 17:15:38 +0800

    commit

---
+++ scripts/scoop/Scoop.ps1
@@ -0,0 +1,72 @@
+# 允许本地运行 PowerShell 脚本.
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# ========================= 变量 Start =========================
+# GitHub Token https://github.com/settings/tokens
+$token = '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'
+# GitHub 代理.
+$mirror = 'https://hub.gitmirror.com'
+# 过滤的软件.
+$filters = @('jdk8', 'pyenv', 'sogou')
+# ========================= 变量 End =========================
+
+function Install-Scoop {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Mirror
+    )
+
+    $install = iwr -useb "$Mirror/https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1"
+    $install -replace 'https://github.com', "$Mirror/https://github.com" | iex
+}
+
+function Install-Apps {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Mirror,
+        [Parameter(Mandatory = $true)]
+        [string]$Token,
+        [string[]]$Filters
+    )
+
+    # 配置仓库源.
+    scoop config scoop_repo "$Mirror/https://github.com/ScoopInstaller/Scoop"
+    # 配置 Token.
+    scoop config gh_token $Token
+    # 安装 Git.
+    $git_manifest = '~\scoop\buckets\main\bucket\git.json'
+    ((Get-Content -Path $git_manifest) -replace 'https://github.com', "$Mirror/https://github.com") | Set-Content -Path $git_manifest
+    scoop install git
+    cmd /c 'mklink /j "%PROGRAMFILES%\Git" "~\scoop\apps\git\current"'
+    # 添加仓库.
+    scoop bucket rm main
+    scoop bucket add main "$Mirror/https://github.com/ScoopInstaller/Main.git"
+    scoop bucket add rainte "$Mirror/https://github.com/rainte/scoop.git"
+    # 添加配置.
+    Copy-Item -Path '~\scoop\buckets\rainte\scripts\git\.gitconfig' -Destination '~\.gitconfig'
+    # 安装软件.
+    scoop install (Get-ChildItem "~\scoop\buckets\rainte\bucket" | ForEach-Object { $_.Name.Replace('.json', '') } | Where-Object { $_ -notin $Filters } | ForEach-Object { 'rainte/' + $_ })
+}
+
+$choice = Read-Host -Prompt '1: Install-Scoop 2: Install-Apps'
+$admin = [security.principal.windowsbuiltinrole]::administrator
+$user = [security.principal.windowsidentity]::getcurrent()
+$isAdmin = ([security.principal.windowsprincipal]($user)).isinrole($admin)
+
+if ($choice -eq 1) {
+    if ($isAdmin) {
+        Write-Error 'Do not start PowerShell as an administrator.' -ErrorAction Stop
+    }
+    else {
+        Install-Scoop -Mirror $mirror
+    }
+}
+
+if ($choice -eq 2) {
+    if ($isAdmin) {
+        Install-Apps -Mirror $mirror -Token $token -Filters $filters
+    }
+    else {
+        Write-Error 'To start PowerShell as an administrator.' -ErrorAction Stop
+    }
+}
